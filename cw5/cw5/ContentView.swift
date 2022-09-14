d84bde3f8f733b5e5deedd5af18bdf972ccffb35
Nour Alhashash <191388@student.kcst.edu.kw>
2022-09-14 19:26:55 +0300

    cw5

---
+++ cw5/cw5/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  cw5
+//
+//  Created by Nour Alhashash on 14/09/2022.
+//
+
+import SwiftUI
+struct ContentView: View {
+    @State var emojis  = ["🥲", "😆", "😂","😡","🥰","😎"]
+    @State  var tap : String = ""
+    var body: some View {
+        VStack {
+            
+            
+            Text("اختار الإيموجي اللي يعبر عنك اليوم")
+                .multilineTextAlignment(.center)
+            
+                .font(.largeTitle)
+            
+            Text(tap)
+                .font(.system(size: 150))
+                .frame(width: 250, height:250)
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            ScrollView(.horizontal){
+                
+                HStack{
+                    
+                    ForEach (emojis, id: \.self){ emoji in
+                        Text (emoji)
+                            .font(.system(size: 150))
+                        
+                            .background(Color.yellow)
+                            .clipShape(Circle())
+                            .onTapGesture{
+                             
+                            tap = emoji
+                                
+                            }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cw5/cw5/cw5App.swift
@@ -0,0 +1,17 @@
+//
+//  cw5App.swift
+//  cw5
+//
+//  Created by Nour Alhashash on 14/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct cw5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
